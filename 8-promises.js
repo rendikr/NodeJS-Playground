9f42859e5acb56f03d3edf4e3fd5edde7761aaac
Rendi K <rendi.krinaldi@gmail.com>
2020-09-30 02:54:59 +0700

    promise chaining

--- 8-promises.js
+++ 8-promises.js
@@ -1,12 +1,29 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject('This is my promise error')
-    // resolve([1, 4, 7])
-  }, 2000)
-})
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 2000)
+  })
+}
 
-doWorkPromise.then((result) => {
-  console.log('Success!', result)
-}).catch((error) => {
-  console.error('Error!', error);
+// add(3, 4).then((sum) => {
+//   console.log(sum
+//     )
+//   add(sum, 5).then((sum2) => {
+//     console.log(sum2)
+//   }).catch((err) => {
+//     console.log(err)
+//   })
+// }).catch((err) => {
+//   console.log(err)
+// })
+
+add(1, 4).then((sum) => {
+  console.log(sum)
+
+  return add(sum, 2) // promise chaining, allow it to run the function and give the result for the next 'then'
+}).then((sum2) => {
+  console.log(sum2)
+}).catch((err) => {
+  console.log(err)
 })
